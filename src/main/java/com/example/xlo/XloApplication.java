029b835eeab767812cc389b39a77a0772c88dd5f
yevhentabachynskyi <tayeiv92@gmail.com>
2022-06-29 23:48:25 +0300

    Initial commit

---
+++ src/main/java/com/example/xlo/XloApplication.java
@@ -0,0 +1,13 @@
+package com.example.xlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xlo/XloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
