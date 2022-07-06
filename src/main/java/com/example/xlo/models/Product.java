facb7a07a9f73b2537ae3621fa994d1cac1aa959
yevhentabachynskyi <tayeiv92@gmail.com>
2022-07-06 16:30:08 +0300

    add upload images

--- src/main/java/com/example/xlo/models/Product.java
+++ src/main/java/com/example/xlo/models/Product.java
@@ -3,6 +3,9 @@ package com.example.xlo.models;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "products")
@@ -28,4 +31,18 @@ public class Product {
 
     @Column(name = "author")
     private String author;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init(){
+        dateOfCreated = LocalDateTime.now();
+    }
+    public void addImageToProduct(Image image) {
+        image.setProduct(this);
+        images.add(image);
+    }
 }
