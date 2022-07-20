24d292a3f45343210cd2f99bcf8f5d53b8e19feb
yevhentabachynskyi <tayeiv92@gmail.com>
2022-07-20 16:59:45 +0300

    add Administrator role

--- src/main/java/com/example/xlo/models/Product.java
+++ src/main/java/com/example/xlo/models/Product.java
@@ -28,13 +28,16 @@ public class Product {
 
     @Column(name = "city")
     private String city;
-
-    @Column(name = "author")
-    private String author;
+//
+//    @Column(name = "author")
+//    private String author;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
     private List<Image> images = new ArrayList<>();
     private Long previewImageId;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinColumn
+    private User user;
     private LocalDateTime dateOfCreated;
 
     @PrePersist
--- src/main/java/com/example/xlo/services/ProductService.java
+++ src/main/java/com/example/xlo/services/ProductService.java
@@ -2,13 +2,16 @@ package com.example.xlo.services;
 
 import com.example.xlo.models.Image;
 import com.example.xlo.models.Product;
+import com.example.xlo.models.User;
 import com.example.xlo.repositories.ProductRepository;
+import com.example.xlo.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +20,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 //    private List<Product> products = new ArrayList<>();
 //    private long ID = 0;
 //    {
@@ -30,7 +34,8 @@ public class ProductService {
         return productRepository.findAll();
     }
 
-    public void saveProduct(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+    public void saveProduct(Principal principal, Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+        product.setUser(getUserByPrincipal(principal));
         Image image1;
         Image image2;
         Image image3;
@@ -47,10 +52,15 @@ public class ProductService {
             image3 = toImageEntity(file3);
             product.addImageToProduct(image3);
         }
-        log.info("Saving new Product. Title: {}; Author: {}", product.getTitle(), product.getAuthor());
+        log.info("Saving new Product. Title: {}; Author: {}", product.getTitle(), product.getUser().getEmail());
         Product productFromDb = productRepository.save(product);
         productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
         productRepository.save(product);
+    }
+
+    public User getUserByPrincipal(Principal principal) {
+        if (principal == null) return new User();
+        return userRepository.findByEmail(principal.getName());
     }
 
     private Image toImageEntity(MultipartFile file) throws IOException {
